c1d06d75185c8a2bda8bf99bc1c4c586b29f0f3a
Xirantics <88784429+Xirantics@users.noreply.github.com>
2021-08-24 13:27:47 +0200

    encapsulation
    Added a script to save the character name if its length is between 1 and 20 characters using get/set

--- OOP Theory/Assets/Scripts/CharacterName.cs
+++ OOP Theory/Assets/Scripts/CharacterName.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterName : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private string characterName;
+
+    private Text errorMessage;
+
+    private void Start()
     {
-        
+        errorMessage = GameObject.FindGameObjectWithTag("Error").GetComponent<Text>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public string CharName
+
     {
-        
+        get { return characterName; }
+
+        set
+        {
+            if (value.Length == 0 || value.Length > 20)
+            {
+                errorMessage.text = "Write a name between 1 and 20 characters!";
+            }
+
+            else
+            {
+                errorMessage.text = "";
+
+                characterName = value;
+                Debug.Log(characterName);
+            }
+        }
     }
+
 }
